6a0437e8aecee6693e6c5e0fe0328ddc4bc6f23d
Tom Jardine-McNamara <tom@itsravenous.com>
2026-04-11 03:21:56 +0100

    apply frame styles to game area

--- map-game/index.php
+++ map-game/index.php
@@ -122,120 +122,122 @@ foreach ($lines as $i => $line) {
     </div>
 
     <?php foreach ($maps as $mapNum => $map): ?>
-      <dialog class="map"
+      <dialog class="map frame"
         id="map-<?php echo $mapNum; ?>">
-        <div
-          class="viewport"
-          tabindex="0"
-          style="--map-width: <?php echo $map['numCols']; ?>; --map-height: <?php echo $map['numRows']; ?>">
-          <?php
-          $index = 1;
-          $doorIdsUsed = [];
-          foreach ($map['rows'] as $row):
-            foreach ($row as $tileChar):
-              $tileDef = $tileTypes[$tileChar];
-              $tileId = null;
+        <div class="frame-inner">
+          <div
+            class="viewport"
+            tabindex="0"
+            style="--map-width: <?php echo $map['numCols']; ?>; --map-height: <?php echo $map['numRows']; ?>">
+            <?php
+            $index = 1;
+            $doorIdsUsed = [];
+            foreach ($map['rows'] as $row):
+              foreach ($row as $tileChar):
+                $tileDef = $tileTypes[$tileChar];
+                $tileId = null;
 
-              // Player start tile on map 1 gets id="start" for splash screen link
-              if ($map['playerStart'] !== null && $index === $map['playerStart']) {
-                $tileId = $mapNum === 1 ? 'start' : "m{$mapNum}-start";
-              }
-
-              // First door tile per destination gets id="m{N}-door-m{M}"
-              if ($tileDef['gotoMap'] !== null) {
-                $doorId = "m{$mapNum}-door-m{$tileDef['gotoMap']}";
-                if (!isset($doorIdsUsed[$doorId])) {
-                  if ($tileId === null) $tileId = $doorId;
-                  $doorIdsUsed[$doorId] = true;
+                // Player start tile on map 1 gets id="start" for splash screen link
+                if ($map['playerStart'] !== null && $index === $map['playerStart']) {
+                  $tileId = $mapNum === 1 ? 'start' : "m{$mapNum}-start";
                 }
-              }
 
-              $tileClasses = ['tile', $tileDef['name']];
-              if ($tileDef['item'] !== null) {
-                array_push($tileClasses, 'item');
-                array_push($tileClasses, "item-" . strtolower($tileDef['item']));
-              }
-              if ($tileDef['encounter'] !== null) {
-                array_push($tileClasses, 'encounter');
-              }
-              if ($tileDef['npc'] !== null) {
-                array_push($tileClasses, 'npc');
-                array_push($tileClasses, 'npc-' . $tileDef['npc']);
-              }
-              if ($tileDef['speech'] !== null) array_push($tileClasses, 'speech');
-              if ($tileDef['gotoMap'] !== null) array_push($tileClasses, 'door');
-          ?>
-              <div
-                class="<?php echo join(' ', $tileClasses); ?>"
-                <?php if ($tileId) echo "id=\"{$tileId}\""; ?>
-                <?php if ($map['playerStart'] !== null && $index === $map['playerStart']): ?>tabindex="0" autofocus<?php endif; ?>>
+                // First door tile per destination gets id="m{N}-door-m{M}"
+                if ($tileDef['gotoMap'] !== null) {
+                  $doorId = "m{$mapNum}-door-m{$tileDef['gotoMap']}";
+                  if (!isset($doorIdsUsed[$doorId])) {
+                    if ($tileId === null) $tileId = $doorId;
+                    $doorIdsUsed[$doorId] = true;
+                  }
+                }
 
-                <?php if ($tileDef['item'] === null): ?><p></p><?php endif; ?>
-                <?php if ($tileDef['gotoMap'] !== null): ?>
-                  <button class="tile-btn" command="show-modal" commandfor="map-<?php echo $tileDef['gotoMap']; ?>">>
-                    Enter ➡️
-                  </button>
-                <?php endif; ?>
+                $tileClasses = ['tile', $tileDef['name']];
+                if ($tileDef['item'] !== null) {
+                  array_push($tileClasses, 'item');
+                  array_push($tileClasses, "item-" . strtolower($tileDef['item']));
+                }
+                if ($tileDef['encounter'] !== null) {
+                  array_push($tileClasses, 'encounter');
+                }
+                if ($tileDef['npc'] !== null) {
+                  array_push($tileClasses, 'npc');
+                  array_push($tileClasses, 'npc-' . $tileDef['npc']);
+                }
+                if ($tileDef['speech'] !== null) array_push($tileClasses, 'speech');
+                if ($tileDef['gotoMap'] !== null) array_push($tileClasses, 'door');
+            ?>
+                <div
+                  class="<?php echo join(' ', $tileClasses); ?>"
+                  <?php if ($tileId) echo "id=\"{$tileId}\""; ?>
+                  <?php if ($map['playerStart'] !== null && $index === $map['playerStart']): ?>tabindex="0" autofocus<?php endif; ?>>
 
-                <?php if ($tileDef['exit'] !== null): ?>
-                  <button class="tile-btn" command="close" commandfor="map-<?php echo $mapNum; ?>">>
-                    Exit ️🚪
-                  </button>
-                <?php endif; ?>
+                  <?php if ($tileDef['item'] === null): ?><p></p><?php endif; ?>
+                  <?php if ($tileDef['gotoMap'] !== null): ?>
+                    <button class="tile-btn" command="show-modal" commandfor="map-<?php echo $tileDef['gotoMap']; ?>">>
+                      Enter ➡️
+                    </button>
+                  <?php endif; ?>
 
-                <?php if ($tileDef['showPuzzle'] !== null): ?>
-                  <button class="tile-btn" command="show-modal" commandfor="puzzle-<?php echo $tileDef['showPuzzle']; ?>">>
-                    Open puzzle 🧩
-                  </button>
-                  <dialog id="puzzle-<?php echo $tileDef['showPuzzle']; ?>">
-                    This is where the puzzle goes!
-                    <input name="orb1" type="range" min="1" max="5" />
-                    <input name="orb2" type="range" min="1" max="5" />
-                    <input name="orb3" type="range" min="1" max="5" />
-                    <button command="close" commandfor="puzzle-<?php echo $tileDef['showPuzzle']; ?>">Close</button>
-                  </dialog>
-                <?php endif; ?>
+                  <?php if ($tileDef['exit'] !== null): ?>
+                    <button class="tile-btn" command="close" commandfor="map-<?php echo $mapNum; ?>">>
+                      Exit ️🚪
+                    </button>
+                  <?php endif; ?>
 
-                <?php if ($tileDef['item'] !== null): ?>
-                  <label class="frame speech-bubble" for="item-<?php echo $tileDef['item']; ?>">
-                    <div class="frame-inner">Pick up <?php echo $tileDef['item']; ?></div>
-                  </label>
-                  <input class="item-checkbox" type="checkbox" id="item-<?php echo $tileDef['item']; ?>" />
-                  <div class="item-icon"></div>
-                <?php endif; ?>
+                  <?php if ($tileDef['showPuzzle'] !== null): ?>
+                    <button class="tile-btn" command="show-modal" commandfor="puzzle-<?php echo $tileDef['showPuzzle']; ?>">>
+                      Open puzzle 🧩
+                    </button>
+                    <dialog id="puzzle-<?php echo $tileDef['showPuzzle']; ?>">
+                      This is where the puzzle goes!
+                      <input name="orb1" type="range" min="1" max="5" />
+                      <input name="orb2" type="range" min="1" max="5" />
+                      <input name="orb3" type="range" min="1" max="5" />
+                      <button command="close" commandfor="puzzle-<?php echo $tileDef['showPuzzle']; ?>">Close</button>
+                    </dialog>
+                  <?php endif; ?>
 
-                <?php if ($tileDef['speech'] !== null): ?>
-                  <div class="frame speech-bubble">
-                    <div class="frame-inner"><?php echo $tileDef['speech']; ?></div>
-                  </div>
-                <?php endif; ?>
+                  <?php if ($tileDef['item'] !== null): ?>
+                    <label class="frame speech-bubble" for="item-<?php echo $tileDef['item']; ?>">
+                      <div class="frame-inner">Pick up <?php echo $tileDef['item']; ?></div>
+                    </label>
+                    <input class="item-checkbox" type="checkbox" id="item-<?php echo $tileDef['item']; ?>" />
+                    <div class="item-icon"></div>
+                  <?php endif; ?>
 
-                <?php if ($tileDef['npc'] !== null): ?>
-                  <div class="npc-sprite"></div>
-                <?php endif; ?>
-
-                <?php if ($tileDef['encounter'] !== null): ?>
-                  <button class="tile-btn" command="show-modal" commandfor="encounter">Ready!</button>
-                  <div class="encounter-transition">A wild T-REX appeared!</div>
-
-                  <dialog class="encounter-container" id="encounter">
-                    Here's the encounter.
-                    <label for="encounter-give">Give her your sandwich</label><input type="checkbox" id="encounter-give" />
-                    <div class="encounter-give-result">
-                      <p>Looks like she's enjoying it! Oh, and look, she's giving you something in return&hellip;</p>
-                      <p><em>Yogaraptor received <b>POWER SPHERE</b></em></p>
-                      <button class="encounter-give-close" command="close" commandfor="encounter">Scuttle away before she changes her mind!</button>
+                  <?php if ($tileDef['speech'] !== null): ?>
+                    <div class="frame speech-bubble">
+                      <div class="frame-inner"><?php echo $tileDef['speech']; ?></div>
                     </div>
-                  </dialog>
-                <?php endif; ?>
-              </div>
-          <?php
-              $index++;
-            endforeach;
-          endforeach; ?>
+                  <?php endif; ?>
 
-          <!-- Player element -->
-          <div class="player"></div>
+                  <?php if ($tileDef['npc'] !== null): ?>
+                    <div class="npc-sprite"></div>
+                  <?php endif; ?>
+
+                  <?php if ($tileDef['encounter'] !== null): ?>
+                    <button class="tile-btn" command="show-modal" commandfor="encounter">Ready!</button>
+                    <div class="encounter-transition">A wild T-REX appeared!</div>
+
+                    <dialog class="encounter-container" id="encounter">
+                      Here's the encounter.
+                      <label for="encounter-give">Give her your sandwich</label><input type="checkbox" id="encounter-give" />
+                      <div class="encounter-give-result">
+                        <p>Looks like she's enjoying it! Oh, and look, she's giving you something in return&hellip;</p>
+                        <p><em>Yogaraptor received <b>POWER SPHERE</b></em></p>
+                        <button class="encounter-give-close" command="close" commandfor="encounter">Scuttle away before she changes her mind!</button>
+                      </div>
+                    </dialog>
+                  <?php endif; ?>
+                </div>
+            <?php
+                $index++;
+              endforeach;
+            endforeach; ?>
+
+            <!-- Player element -->
+            <div class="player"></div>
+          </div>
         </div>
       </dialog>
     <?php endforeach; ?>
