ce16995a62db6307fbb930ac3897b206dc6acbb8
Tom Jardine-McNamara <tom@itsravenous.com>
2026-04-11 03:52:27 +0100

    add pixel font

--- map-game/index.php
+++ map-game/index.php
@@ -97,6 +97,9 @@ foreach ($lines as $i => $line) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>CSS RPG</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="styles/main.css" />
   <link rel="stylesheet" type="text/css" href="styles/tiles.css" />
   <link rel="stylesheet" type="text/css" href="styles/player.css" />
