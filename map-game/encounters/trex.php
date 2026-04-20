3f31d1bfe823c7a33499389761eb7a64f47a84b4
Tom Jardine-McNamara <tom@itsravenous.com>
2026-04-20 14:55:21 +0100

    wip skin the encounter

--- map-game/encounters/trex.php
+++ map-game/encounters/trex.php
@@ -1,7 +1,33 @@
-Here's the encounter.
-<label for="encounter-give">Give her your sandwich</label><input type="checkbox" id="encounter-give" />
-<div class="encounter-give-result">
-  <p>Looks like she's enjoying it! Oh, and look, she's giving you something in return&hellip;</p>
-  <p><em>Yogaraptor received <b>POWER SPHERE</b></em></p>
-  <button class="encounter-give-close" command="close" commandfor="encounter-trex">Scuttle away before she changes her mind!</button>
+<div class="encounter-scene">
+  <div class="encounter-area">
+    <div class="encounter-player"></div>
+    <div class="encounter-enemy">🦖</div>
+  </div>
+
+  <div class="encounter-dialogue">
+    <div class="frame encounter-speech encounter-speech-1">
+      <div class="frame-inner">The T-Rex is growling. She sounds hungry!</div>
+    </div>
+
+    <label for="encounter-give" class="frame encounter-speech encounter-action">
+      <span class="frame-inner">Give her your croissant? 🥐</span>
+    </label>
+    <input type="checkbox" id="encounter-give" />
+
+    <div class="encounter-give-result">
+      <div class="encounter-croissant">🥐</div>
+
+      <div class="frame encounter-speech encounter-speech-2">
+        <div class="frame-inner">She looks happy! She's giving you something&hellip;</div>
+      </div>
+
+      <div class="frame encounter-speech encounter-speech-3">
+        <div class="frame-inner"><em>Yogaraptor received <b>STABILISER ROD</b></em></div>
+      </div>
+
+      <button class="frame encounter-give-close" command="close" commandfor="encounter-trex">
+        <span class="frame-inner">Scuttle away before she changes her mind!</span>
+      </button>
+    </div>
+  </div>
 </div>
