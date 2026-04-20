70080ada59a2596455dd923f0abc0e277d936c2b
Tom Jardine-McNamara <tom@itsravenous.com>
2026-04-20 22:37:56 +0100

    polish ui

--- map-game/encounters/stego.php
+++ map-game/encounters/stego.php
@@ -4,30 +4,30 @@
       <div class="encounter-enemy-sprite"></div>
     </div>
     <div class="encounter-player"></div>
+    <div class="encounter-croissant"></div>
   </div>
 
   <div class="encounter-dialogue">
-    <div class="frame encounter-speech encounter-speech-1">
-      <div class="frame-inner">The Stego is growling. She sounds hungry!</div>
+    <div class="encounter-speech encounter-speech-1">
+      The Stego is growling. She sounds hungry!
     </div>
 
-    <label for="encounter-give" class="frame encounter-speech encounter-action">
+    <label for="encounter-give" class="tile-btn encounter-speech encounter-action">
       <span class="frame-inner">Give her your croissant?</span>
     </label>
     <input type="checkbox" id="encounter-give" />
 
     <div class="encounter-give-result">
-      <div class="encounter-croissant"></div>
 
       <div class="encounter-speech encounter-speech-2">
         She looks happy! She's giving you something&hellip;
       </div>
 
       <div class="encounter-speech encounter-speech-3">
-        <em>Yogaraptor received <b>STABILISER ROD</b></em>
+        Yogaraptor received <b>STABILISER ROD</b>!
       </div>
 
-      <button class="frame encounter-give-close" command="close" commandfor="encounter-trex">
+      <button class="tile-btn encounter-give-close" command="close" commandfor="encounter-stego">
         <span class="frame-inner">Scuttle away before she changes her mind!</span>
       </button>
     </div>
