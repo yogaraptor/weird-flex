d44a8925507ef5f4d20ae2fef7c680da1e13258b
Tom Jardine-McNamara <tom@itsravenous.com>
2026-04-30 20:39:08 +0100

    add tips to splash

--- map-game/splash.php
+++ map-game/splash.php
@@ -2,9 +2,17 @@
   <div class="npc-wrapper npc-wrapper-yogaraptor">
     <div class="npc-sprite"></div>
   </div>
+
   <h1>Yogaraptor presents:<span>CSS Quest!</span></h1>
+
   <div class="splash-screen-actions">
     <button class="frame button" command="show-modal" commandfor="map-10"><span class="frame-inner">Start game</span></button>
     <button class="frame button" command="show-modal" commandfor="credits"><span class="frame-inner">View credits</span></button>
+  </div>
+
+  <div class="splash-screen-tips">
+    <h2>Tips</h2>
+    <p>Use arrow keys to move. Holding them down can be janky, try tapping them instead.</p>
+    <p>Try not to press <b>tab</b> or <b>esc</b></p>
   </div>
 </div>
